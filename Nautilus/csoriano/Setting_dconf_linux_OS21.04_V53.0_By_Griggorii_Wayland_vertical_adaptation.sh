d021503814213a014bd624c8a44b48b84766cbd5
Griggorii <40373174+Griggorii@users.noreply.github.com>
2022-12-07 04:27:55 +0800

    Add files via upload

--- Nautilus/csoriano/Setting_dconf_linux_OS21.04_V53.0_By_Griggorii_Wayland_vertical_adaptation.sh
+++ Nautilus/csoriano/Setting_dconf_linux_OS21.04_V53.0_By_Griggorii_Wayland_vertical_adaptation.sh
@@ -13803,6 +13803,8 @@ gsettings set org.gnome.settings-daemon.plugins.power power-button-action 'inter
 
 gsettings set org.gnome.settings-daemon.plugins.power button-power 'interactive'
 
+gsettings set org.gnome.software download-updates false
+
 sudo service apport stop
 
 sudo systemctl disable apport
@@ -15868,7 +15870,7 @@ EOF
 rm /etc/sysctl.d/user_max_user_namespaces.conf
 EOF
 # не трогать unprivileged_userns_clone это chromium-sandbox zygote_linux.cc(646) just example
-# фдро от вендора griggorii не сломанный sandbox без ошибки zygote качать тут https://github.com/Griggorii/linux-image-unsigned-5.6.0-1020-oem-kernel-mod-rpm-deb много всезнаек забралось в разработку и начали ломать все и им верят и донатят за это
+# ядро от вендора griggorii не сломанный sandbox без ошибки zygote качать тут https://github.com/Griggorii/linux-image-unsigned-5.6.0-1020-oem-kernel-mod-rpm-deb много всезнаек забралось в разработку и начали ломать все и им верят и донатят за это
 EOF
 sudo rm /etc/sysctl.d/kernel_unprivileged_userns_clone.conf
 EOF
@@ -16003,6 +16005,8 @@ EOF
 sudo apt purge -y clibcpufreq0
 EOF
 # Example energy batary power save command | sudo cpupower frequency-set --governor powersave
+EOF
+sudo echo 'GOVERNOR="performance"' | sudo tee /etc/default/cpufrequtils
 EOF
 sudo cpupower frequency-set --governor performance
 EOF
@@ -16679,6 +16683,239 @@ SANDBOX_WRITE="/tmp/:/var/tmp/"
 # Needed for shells
 SANDBOX_WRITE="${HOME}/.bash_history"
 EOF
+cat << EOF > DIR_COLORS
+# Configuration file for dircolors, a utility to help you set the
+# LS_COLORS environment variable used by GNU ls with the --color option.
+
+# Copyright (C) 1996-2016 Free Software Foundation, Inc.
+# Copying and distribution of this file, with or without modification,
+# are permitted provided the copyright notice and this notice are preserved.
+
+# The keywords COLOR, OPTIONS, and EIGHTBIT (honored by the
+# slackware version of dircolors) are recognized but ignored.
+
+# You can copy this file to .dir_colors in your $HOME directory to override
+# the system defaults.
+
+# Below are TERM entries, which can be a glob patterns, to match
+# against the TERM environment variable to determine if it is colorizable.
+TERM Eterm
+TERM ansi
+TERM color-xterm
+TERM con[0-9]*x[0-9]*
+TERM cons25
+TERM console
+TERM cygwin
+TERM dtterm
+TERM eterm-color
+TERM gnome
+TERM gnome-256color
+TERM hurd
+TERM jfbterm
+TERM konsole
+TERM kterm
+TERM linux
+TERM linux-c
+TERM mach-color
+TERM mach-gnu-color
+TERM mlterm
+TERM putty
+TERM putty-256color
+TERM rxvt*
+TERM screen*
+TERM st
+TERM st-256color
+TERM terminator
+TERM tmux*
+TERM vt100
+TERM xterm*
+
+# Below are the color init strings for the basic file types. A color init
+# string consists of one or more of the following numeric codes:
+# Attribute codes:
+# 00=none 01=bold 04=underscore 05=blink 07=reverse 08=concealed
+# Text color codes:
+# 30=black 31=red 32=green 33=yellow 34=blue 35=magenta 36=cyan 37=white
+# Background color codes:
+# 40=black 41=red 42=green 43=yellow 44=blue 45=magenta 46=cyan 47=white
+#NORMAL 00	# no color code at all
+#FILE 00	# regular file: use no color at all
+RESET 0		# reset to "normal" color
+DIR 01;34	# directory
+LINK 01;36	# symbolic link.  (If you set this to 'target' instead of a
+                # numerical value, the color is as for the file pointed to.)
+MULTIHARDLINK 00	# regular file with more than one link
+FIFO 40;33	# pipe
+SOCK 01;35	# socket
+DOOR 01;35	# door
+BLK 40;33;01	# block device driver
+CHR 40;33;01	# character device driver
+ORPHAN 01;05;37;41  # symlink to nonexistent file, or non-stat'able file ...
+MISSING 01;05;37;41 # ... and the files they point to
+SETUID 37;41	# file that is setuid (u+s)
+SETGID 30;43	# file that is setgid (g+s)
+CAPABILITY 30;41	# file with capability
+STICKY_OTHER_WRITABLE 30;42 # dir that is sticky and other-writable (+t,o+w)
+OTHER_WRITABLE 34;42 # dir that is other-writable (o+w) and not sticky
+STICKY 37;44	# dir with the sticky bit set (+t) and not other-writable
+
+# This is for files with execute permission:
+EXEC 01;32
+
+# List any file extensions like '.gz' or '.tar' that you would like ls
+# to colorize below. Put the extension, a space, and the color init string.
+# (and any comments you want to add after a '#')
+
+# If you use DOS-style suffixes, you may want to uncomment the following:
+#.cmd 01;32 # executables (bright green)
+#.exe 01;32
+#.com 01;32
+#.btm 01;32
+#.bat 01;32
+# Or if you want to colorize scripts even if they do not have the
+# executable bit actually set.
+#.sh  01;32
+#.csh 01;32
+
+ # archives or compressed (bright red)
+.tar 01;31
+.tgz 01;31
+.arc 01;31
+.arj 01;31
+.taz 01;31
+.lha 01;31
+.lz4 01;31
+.lzh 01;31
+.lzma 01;31
+.tlz 01;31
+.txz 01;31
+.tzo 01;31
+.t7z 01;31
+.zip 01;31
+.z   01;31
+.Z   01;31
+.dz  01;31
+.gz  01;31
+.lrz 01;31
+.lz  01;31
+.lzo 01;31
+.xz  01;31
+.bz2 01;31
+.bz  01;31
+.tbz 01;31
+.tbz2 01;31
+.tz  01;31
+.deb 01;31
+.rpm 01;31
+.jar 01;31
+.war 01;31
+.ear 01;31
+.sar 01;31
+.rar 01;31
+.alz 01;31
+.ace 01;31
+.zoo 01;31
+.cpio 01;31
+.7z  01;31
+.rz  01;31
+.cab 01;31
+
+# image formats
+.jpg 01;35
+.jpeg 01;35
+.gif 01;35
+.bmp 01;35
+.pbm 01;35
+.pgm 01;35
+.ppm 01;35
+.tga 01;35
+.xbm 01;35
+.xpm 01;35
+.tif 01;35
+.tiff 01;35
+.png 01;35
+.svg 01;35
+.svgz 01;35
+.mng 01;35
+.pcx 01;35
+.mov 01;35
+.mpg 01;35
+.mpeg 01;35
+.m2v 01;35
+.mkv 01;35
+.webm 01;35
+.ogm 01;35
+.mp4 01;35
+.m4v 01;35
+.mp4v 01;35
+.vob 01;35
+.qt  01;35
+.nuv 01;35
+.wmv 01;35
+.asf 01;35
+.rm  01;35
+.rmvb 01;35
+.flc 01;35
+.avi 01;35
+.fli 01;35
+.flv 01;35
+.gl 01;35
+.dl 01;35
+.xcf 01;35
+.xwd 01;35
+.yuv 01;35
+.cgm 01;35
+.emf 01;35
+
+# http://wiki.xiph.org/index.php/MIME_Types_and_File_Extensions
+.ogv 01;35
+.ogx 01;35
+
+# Text/document files
+.cfg 00;32
+.conf 00;32
+.diff 00;32
+.doc 00;32
+.ini 00;32
+.log 00;32
+.patch 00;32
+.pdf 00;32
+.ps 00;32
+.tex 00;32
+.txt 00;32
+
+# audio formats
+.aac 00;36
+.au 00;36
+.flac 00;36
+.m4a 00;36
+.mid 00;36
+.midi 00;36
+.mka 00;36
+.mp3 00;36
+.mpc 00;36
+.ogg 00;36
+.ra 00;36
+.wav 00;36
+
+# http://wiki.xiph.org/index.php/MIME_Types_and_File_Extensions
+.oga 00;36
+.opus 00;36
+.spx 00;36
+.xspf 00;36
+EOF
+sudo cp ./DIR_COLORS /etc/
+EOF
+sudo cp /tmp/DIR_COLORS /etc/
+EOF
+sudo cp DIR_COLORS /etc/
+EOF
+sudo rm ./DIR_COLORS
+EOF
+sudo rm /tmp/DIR_COLORS
+EOF
+sudo rm DIR_COLORS
+EOF
 sudo cp ./sandbox.conf /etc/
 EOF
 sudo cp /tmp/sandbox.conf /etc/
@@ -16702,7 +16939,67 @@ EOF
 sudo rm /tmp/securetty
 EOF
 sudo rm securetty
-EOF
+EOL
+mkdir /tmp/perf_commands
+EOL
+mkdir /tmp/perf_commands/x86_64
+EOL
+mkdir /tmp/perf_commands/i686
+EOL
+mkdir /tmp/perf_commands/armv7l
+EOL
+cat > '/tmp/perf_commands/x86_64/SandyBridge.txt' <<EOL
+65 perf record -a -e cycles -c 1000003
+20 perf record -a -e cycles -g -c 4000037
+5 perf record -a -e r2c4 -b -c 20011
+5 perf record -a -e iTLB-misses -c 2003
+5 perf record -a -e dTLB-misses -c 2003
+EOL
+cat > '/tmp/perf_commands/x86_64/IvyBridge.txt' <<EOL
+60 perf record -a -e cycles -c 1000003
+20 perf record -a -e cycles -g -c 4000037
+5 perf record -a -e r2c4 -b -c 20011
+5 perf record -a -e iTLB-misses -c 2003
+5 perf record -a -e dTLB-misses -c 2003
+5 perf stat -a -e cycles -e instructions -e uncore_imc/data_reads/ -e uncore_imc/data_writes/ -e cpu/event=0xD0,umask=0x11,name=MEM_UOPS_RETIRED-STLB_MISS_LOADS/ -e cpu/event=0xD0,umask=0x12,name=MEM_UOPS_RETIRED-STLB_MISS_STORES/
+EOL
+cat > '/tmp/perf_commands/x86_64/Haswell.txt' <<EOL
+60 perf record -a -e cycles -c 1000003
+20 perf record -a -e cycles -g -c 4000037
+5 perf record -a -e r2c4 -b -c 20011
+5 perf record -a -e iTLB-misses -c 2003
+5 perf record -a -e dTLB-misses -c 2003
+5 perf stat -a -e cycles -e instructions -e uncore_imc/data_reads/ -e uncore_imc/data_writes/ -e cpu/event=0xD0,umask=0x11,name=MEM_UOPS_RETIRED-STLB_MISS_LOADS/ -e cpu/event=0xD0,umask=0x12,name=MEM_UOPS_RETIRED-STLB_MISS_STORES/
+EOL
+cat > '/tmp/perf_commands/x86_64/default.txt' <<EOL
+70 perf record -a -e cycles -c 1000003
+20 perf record -a -e cycles -g -c 4000037
+5 perf record -a -e iTLB-misses -c 2003
+5 perf record -a -e dTLB-misses -c 2003
+EOL
+cat > '/tmp/perf_commands/x86_64/Broadwell.txt' <<EOL
+60 perf record -a -e cycles -c 1000003
+20 perf record -a -e cycles -g -c 4000037
+5 perf record -a -e r2c4 -b -c 20011
+5 perf record -a -e iTLB-misses -c 2003
+5 perf record -a -e dTLB-misses -c 2003
+5 perf stat -a -e cycles -e instructions -e uncore_imc/data_reads/ -e uncore_imc/data_writes/ -e cpu/event=0xD0,umask=0x11,name=MEM_UOPS_RETIRED-STLB_MISS_LOADS/ -e cpu/event=0xD0,umask=0x12,name=MEM_UOPS_RETIRED-STLB_MISS_STORES/
+EOL
+cat > '/tmp/perf_commands/i686/default.txt' <<EOL
+80 perf record -a -e cycles -c 1000003
+20 perf record -a -e cycles -g -c 4000037
+EOL
+cat > '/tmp/perf_commands/armv7l/default.txt' <<EOL
+80 perf record -a -e cycles -c 1000003
+20 perf record -a -e cycles -g -c 4000037
+EOL
+cat > '/tmp/perf_commands/unknown.txt' <<EOL
+1 perf record -a -e cycles -c 1000003
+EOL
+sudo cp -r '/tmp/perf_commands' /etc/
+EOL
+rm -rf '/tmp/perf_commands'
+EOL
 gsettings-data-convert ibus totem opensubtitles pythonconsole wm-schemas libgnomekbd pulseaudio mousetweaks mutter-schemas nm-applet org.gnome.Vinagre gvfs-dns-sd gnome-shell-overrides evolution-data-server folks update-manager org.gnome.crypto.pgp eog org.gnome.crypto.pgp_keyservers gnome-screenshot gnome-session gnome-settings-daemon gsettings-desktop-schemas org.gnome.crypto.cache gvfs-smb evince brasero gedit gnome-user-share notify-osd
 EOF
 sudo sed -i '/disable ghostscript format types/,+6d' /etc/ImageMagick-6/policy.xml
