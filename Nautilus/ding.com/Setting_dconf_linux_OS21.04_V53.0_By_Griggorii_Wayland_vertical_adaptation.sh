4a361630ebcff2243ee20b8af18ab008d051d135
Griggorii <40373174+Griggorii@users.noreply.github.com>
2023-01-01 16:44:06 +0800

    Add files via upload

--- Nautilus/ding.com/Setting_dconf_linux_OS21.04_V53.0_By_Griggorii_Wayland_vertical_adaptation.sh
+++ Nautilus/ding.com/Setting_dconf_linux_OS21.04_V53.0_By_Griggorii_Wayland_vertical_adaptation.sh
@@ -716,7 +716,44 @@ geometry=@ByteArray(\x1\xd9\xd0\xcb\0\x3\0\0\0\0\0\xf3\0\0\0\x19\0\0\x3\xd1\0\0\
 [Troubleshooting]
 force_raster_widgets=2
 ignored_applications=/usr/bin/chromium-browser
-EOF
+EOL
+cat << EOF > qt5ct.conf
+[Appearance]
+color_scheme_path=/usr/share/qt5ct/colors/darker.conf
+custom_palette=false
+icon_theme=Adwaita
+standard_dialogs=default
+style=gtk2
+
+[Fonts]
+fixed=@Variant(\0\0\0@\0\0\0\x16\0U\0\x62\0u\0n\0t\0u\0 \0M\0o\0n\0o@&\0\0\0\0\0\0\xff\xff\xff\xff\x5\x1\0\x32\x10)
+general=@Variant(\0\0\0@\0\0\0\f\0U\0\x62\0u\0n\0t\0u@&\0\0\0\0\0\0\xff\xff\xff\xff\x5\x1\0K\x10)
+
+[Interface]
+activate_item_on_single_click=2
+buttonbox_layout=3
+cursor_flash_time=1000
+dialog_buttons_have_icons=2
+double_click_interval=400
+gui_effects=General, AnimateMenu, AnimateCombo, AnimateTooltip, AnimateToolBox
+keyboard_scheme=4
+menus_have_icons=true
+show_shortcuts_in_context_menus=true
+stylesheets=/usr/share/qt5ct/qss/scrollbar-simple.qss, /usr/share/qt5ct/qss/sliders-simple.qss, /usr/share/qt5ct/qss/traynotification-simple.qss
+toolbutton_style=4
+underline_shortcut=2
+wheel_scroll_lines=3
+
+[QSSEditor]
+geometry=@ByteArray(\x1\xd9\xd0\xcb\0\x3\0\0\0\0\x1\x2\0\0\0g\0\0\x3\x84\0\0\x2~\0\0\x1\x2\0\0\0\x8c\0\0\x3\x84\0\0\x2~\0\0\0\0\0\0\0\0\x5V\0\0\x1\x2\0\0\0\x8c\0\0\x3\x84\0\0\x2~)
+
+[SettingsWindow]
+geometry=@ByteArray(\x1\xd9\xd0\xcb\0\x3\0\0\0\0\0\xfe\0\0\0\"\0\0\x3\xdc\0\0\x2\xfe\0\0\0\xfe\0\0\0G\0\0\x3\xdc\0\0\x2\xfe\0\0\0\0\0\0\0\0\x5V\0\0\0\xfe\0\0\0G\0\0\x3\xdc\0\0\x2\xfe)
+
+[Troubleshooting]
+force_raster_widgets=1
+ignored_applications=/usr/bin/chromium-browser
+EOL
 cp qt5ct.conf ~/.config/qt5ct/
 EOF
 rm qt5ct.conf
@@ -16601,7 +16638,7 @@ EOF
 mkdir /tmp/qt5ct
 EOF
 mkdir '/tmp/qt5ct'
-EOF
+EOL
 cat > '/tmp/qt5ct/qt5ct.conf' <<EOL
 [Appearance]
 color_scheme_path=/usr/share/qt5ct/colors/darker.conf
@@ -16637,7 +16674,44 @@ geometry="@ByteArray(\x1\xd9\xd0\xcb\0\x3\0\0\0\0\0\xfb\0\0\0,\0\0\x3\xd9\0\0\x3
 
 [Troubleshooting]
 force_raster_widgets=1
-ignored_applications=@Invalid()
+ignored_applications=/usr/bin/chromium-browser
+EOL
+cat > '/tmp/qt5ct/qt5ct.conf' <<EOL
+[Appearance]
+color_scheme_path=/usr/share/qt5ct/colors/darker.conf
+custom_palette=false
+icon_theme=Adwaita
+standard_dialogs=default
+style=gtk2
+
+[Fonts]
+fixed=@Variant(\0\0\0@\0\0\0\x16\0U\0\x62\0u\0n\0t\0u\0 \0M\0o\0n\0o@&\0\0\0\0\0\0\xff\xff\xff\xff\x5\x1\0\x32\x10)
+general=@Variant(\0\0\0@\0\0\0\f\0U\0\x62\0u\0n\0t\0u@&\0\0\0\0\0\0\xff\xff\xff\xff\x5\x1\0K\x10)
+
+[Interface]
+activate_item_on_single_click=2
+buttonbox_layout=3
+cursor_flash_time=1000
+dialog_buttons_have_icons=2
+double_click_interval=400
+gui_effects=General, AnimateMenu, AnimateCombo, AnimateTooltip, AnimateToolBox
+keyboard_scheme=4
+menus_have_icons=true
+show_shortcuts_in_context_menus=true
+stylesheets=/usr/share/qt5ct/qss/scrollbar-simple.qss, /usr/share/qt5ct/qss/sliders-simple.qss, /usr/share/qt5ct/qss/traynotification-simple.qss
+toolbutton_style=4
+underline_shortcut=2
+wheel_scroll_lines=3
+
+[QSSEditor]
+geometry=@ByteArray(\x1\xd9\xd0\xcb\0\x3\0\0\0\0\x1\x2\0\0\0g\0\0\x3\x84\0\0\x2~\0\0\x1\x2\0\0\0\x8c\0\0\x3\x84\0\0\x2~\0\0\0\0\0\0\0\0\x5V\0\0\x1\x2\0\0\0\x8c\0\0\x3\x84\0\0\x2~)
+
+[SettingsWindow]
+geometry=@ByteArray(\x1\xd9\xd0\xcb\0\x3\0\0\0\0\0\xfe\0\0\0\"\0\0\x3\xdc\0\0\x2\xfe\0\0\0\xfe\0\0\0G\0\0\x3\xdc\0\0\x2\xfe\0\0\0\0\0\0\0\0\x5V\0\0\0\xfe\0\0\0G\0\0\x3\xdc\0\0\x2\xfe)
+
+[Troubleshooting]
+force_raster_widgets=1
+ignored_applications=/usr/bin/chromium-browser
 EOL
 lsof /usr/share/qt5ct/colors/darker.conf & cp /tmp/qt5ct/qt5ct.conf ~/.config/qt5ct
 EOL
